3a329873bac55d3af245f6caefc8c72c5e43c7c2
waasifmhafeez <wasifhafeez26@gmail.com>
2024-06-10 21:20:18 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+console.log(process.env);
+const express = require("express");
+
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Waasif..");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("waasifhafeezdotcom...");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Login at Chai or Code </h1>");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2>Chai or Code...</h2>");
+});
+
+// app.listen(port, () => {
+//     console.log(`Listeng app on port : ${port}`);
+// });
+app.listen(process.env.Port, () => {
+  console.log(`Listeng app on port : ${port}`);
+});
